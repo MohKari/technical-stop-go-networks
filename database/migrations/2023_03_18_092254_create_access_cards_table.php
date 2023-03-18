36d374d44f2c57a1b11d52661e93d19bf678b885
Mohammed Karim <mkarim@workforcesoftware.com>
2023-03-18 09:42:08 +0000

    Add base level tables.

---
+++ database/migrations/2023_03_18_092254_create_access_cards_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('access_cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('rfid',32);
+            $table->foreignId('employee_id')->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('access_cards');
+    }
+};
