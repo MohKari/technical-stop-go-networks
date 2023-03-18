139d0e6cb5879865ffba621ac76816e0dad967e2
Mohammed Karim <mkarim@workforcesoftware.com>
2023-03-18 09:54:41 +0000

    Woops, forgot to make rfid unique.

--- database/migrations/2023_03_18_092254_create_access_cards_table.php
+++ database/migrations/2023_03_18_092254_create_access_cards_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('access_cards', function (Blueprint $table) {
             $table->id();
-            $table->string('rfid',32);
+            $table->string('rfid',32)->unique();
             $table->foreignId('employee_id')->constrained();
             $table->timestamps();
             $table->softDeletes();
