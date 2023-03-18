0eb245cf3624d26d2c2b446a8744b49cbc1cc3de
Mohammed Karim <mkarim@workforcesoftware.com>
2023-03-18 10:17:57 +0000

    Seed 10 random employees.

---
+++ database/seeders/Demo.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Employee;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class Demo extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->addEmployees();
+    }
+
+    private function addEmployees(): void
+    {
+        Employee::factory()
+            ->count(10)
+            ->create();
+    }
+}
