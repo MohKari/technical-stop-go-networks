a5b1b0c64edf07276dc6cb5fd8e8de8cb77b7a45
Mohammed Karim <mkarim@workforcesoftware.com>
2023-03-18 10:39:50 +0000

    Change to int from string.

--- database/migrations/2023_03_18_092254_create_access_cards_table.php
+++ database/migrations/2023_03_18_092254_create_access_cards_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('access_cards', function (Blueprint $table) {
             $table->id();
             $table->string('rfid',32)->unique();
-            $table->foreignId('employee_id')->constrained();
+            $table->integer('employee_id');
             $table->timestamps();
             $table->softDeletes();
         });
